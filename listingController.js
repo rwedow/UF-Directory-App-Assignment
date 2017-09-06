33156321c03bf6298ee1634c1e685ce9a407d4c8
Rebecca Wedow <rwedow@ufl.edu>
2017-09-06 12:12:39 -0400

    first commit

--- listingController.js
+++ listingController.js
@@ -1,14 +1,18 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function(newL) {
+      $scope.listings.push(newL);
+    };
+    $scope.deleteListing = function(listing) {
+      if(listing == $scope.detailedInfo) {
+        $scope.detailedInfo = undefined;
+      }
+      $scope.listings.splice($scope.listings.indexOf(listing), 1);
+    };
+    $scope.showDetails = function(listing) {
+  	   $scope.detailedInfo = listing;
+	  };
   }
 ]);
